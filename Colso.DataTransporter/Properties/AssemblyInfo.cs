9fd00830ffa7eab62ad3ddc683659794ac22af3d
Unknown <ryuk67@gmail.com>
2017-03-23 14:28:04 -0400

    Added Support for ActivityParty Mapping

--- Colso.DataTransporter/Properties/AssemblyInfo.cs
+++ Colso.DataTransporter/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("aef1b9bd-4f00-4fed-bbba-f72cddc61de3")]
 
-[assembly: AssemblyVersion("1.2017.2.4")]
-[assembly: AssemblyFileVersion("1.2017.2.4")]
+[assembly: AssemblyVersion("1.2017.2.5")]
+[assembly: AssemblyFileVersion("1.2017.2.5")]

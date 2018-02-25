ddd6662908cee37e9a7130824453231d0a56389c
Markus Erlandsson <markus.erlandsson@outlook.com>
2018-02-25 22:08:27 +0100

    Draft, support of change state, tested on single record

---
+++ Colso.DataTransporter/Models/RecordStateAndStatus.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colso.Xrm.DataTransporter.Models
+{
+    class RecordStateAndStatus
+    {
+        public OptionSetValue State { get; set; }
+        public OptionSetValue Status { get; set; }
+    }
+}
